e780b0b59e791c98122cc1bf427ff3c6e76a44b6
TayKaiJun <tehohno@gmail.com>
2024-01-24 18:44:36 -0800

    forgot to copy this

--- audio_and_led.py
+++ audio_and_led.py
@@ -46,6 +46,17 @@ note_to_led_index = {
     'C8': 87
 }
 
+def wheel(pos):
+    """Generate rainbow colors across 0-255 positions."""
+    if pos < 85:
+        return Color(pos * 3, 255 - pos * 3, 0)
+    elif pos < 170:
+        pos -= 85
+        return Color(255 - pos * 3, 0, pos * 3)
+    else:
+        pos -= 170
+        return Color(0, pos * 3, 255 - pos * 3)
+
 def rainbow(strip, wait_ms=20, iterations=1):
     """Draw rainbow that fades across all pixels at once."""
     for j in range(256*iterations):
