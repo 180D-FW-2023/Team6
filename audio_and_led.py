b6f1a7f07586e6dbc317a3113ed0486cd5788dd4
Kai Jun Tay <kaijuntay687@g.ucla.edu>
2024-01-28 00:35:18 +0000

    receives scale correctly, added new dict to convert web input

--- audio_and_led.py
+++ audio_and_led.py
@@ -46,6 +46,18 @@ note_to_led_index = {
     'C8': 87
 }
 
+note_to_led_index_web = {
+    'A0': 0, 'A#0': 1, 'B0': 2,
+    'C1': 3, 'C#1': 4, 'D1': 5, 'D#1': 6, 'E1': 7, 'F1': 8, 'F#1': 9, 'G1': 10, 'G#1': 11, 'A1': 12, 'A#1': 13, 'B1': 14,
+    'C2': 15, 'C#2': 16, 'D2': 17, 'D#2': 18, 'E2': 19, 'F2': 20, 'F#2': 21, 'G2': 22, 'G#2': 23, 'A2': 24, 'A#2': 25, 'B2': 26,
+    'C3': 27, 'C#3': 28, 'D3': 29, 'D#3': 30, 'E3': 31, 'F3': 32, 'F#3': 33, 'G3': 34, 'G#3': 35, 'A3': 36, 'A#3': 37, 'B3': 38,
+    'C4': 39, 'C#4': 40, 'D4': 41, 'D#4': 42, 'E4': 43, 'F4': 44, 'F#4': 45, 'G4': 46, 'G#4': 47, 'A4': 48, 'A#4': 49, 'B4': 50,
+    'C5': 51, 'C#5': 52, 'D5': 53, 'D#5': 54, 'E5': 55, 'F5': 56, 'F#5': 57, 'G5': 58, 'G#5': 59, 'A5': 60, 'A#5': 61, 'B5': 62,
+    'C6': 63, 'C#6': 64, 'D6': 65, 'D#6': 66, 'E6': 67, 'F6': 68, 'F#6': 69, 'G6': 70, 'G#6': 71, 'A6': 72, 'A#6': 73, 'B6': 74,
+    'C7': 75, 'C#7': 76, 'D7': 77, 'D#7': 78, 'E7': 79, 'F7': 80, 'F#7': 81, 'G7': 82, 'G#7': 83, 'A7': 84, 'A#7': 85, 'B7': 86,
+    'C8': 87
+}
+
 def wheel(pos):
     """Generate rainbow colors across 0-255 positions."""
     if pos < 85:
@@ -94,7 +106,6 @@ i = 0
 prev_l = ""
 onsets = np.array([])
 target_notes = []
-# target_notes = ['E3', 'F♯3', 'G♯3', 'A3', 'B3', 'C♯4', 'D♯4', 'E4']
 played_notes = []
 
 ################## MQTT SETUP #####################
@@ -117,6 +128,8 @@ def on_disconnect(client, userdata, rc):
 def on_message(client, userdata, message):
     global target_notes
     
+    global target_notes
+    
     colorWipeAll(strip, Color(128,0,0), wait_ms=50)
     colorWipeAll(strip, Color(0,128,0), wait_ms=50)
     colorWipeAll(strip, Color(0,0,128), wait_ms=50)
@@ -131,7 +144,7 @@ def on_message(client, userdata, message):
         played_notes = []
         print(target_notes)
         # set first color to blue
-        setColorByIndices(strip, note_to_led_index[target_notes[0]], color=Color(0,0,128),wait_ms=50)
+        setColorByIndices(strip, note_to_led_index_web[target_notes[0]], color=Color(0,0,128),wait_ms=50)
     
 # 1. create a client instance.
 client = mqtt.Client()
@@ -154,7 +167,6 @@ print("Initializing LED strip")
 rainbow(strip, wait_ms=20, iterations=1)
 colorWipeAll(strip, Color(0,0,0), wait_ms=50)
 
-print("FIRST !!!!!" , target_notes)
 
 while True:
     #record audio buffer
@@ -199,6 +211,7 @@ while True:
     if max_noise < 100:
         l = ""
     # print(l)
+    # print(l)
     
     prev_l = l
 
@@ -208,11 +221,14 @@ while True:
     if top_note:
         print("TARGET ", target_notes)
         print("RECORDED ", played_notes)
+        print("TARGET ", target_notes)
+        print("RECORDED ", played_notes)
         print("Curr Note: ", top_note)
         
         if target_notes and len(played_notes) < len(target_notes):
             if top_note == target_notes[len(played_notes)]:
                 print("Correct!")
+                setColorByIndices(strip, note_to_led_index[top_note], color=Color(0,128,0),wait_ms=1000)
                 setColorByIndices(strip, note_to_led_index[top_note], color=Color(0,128,0),wait_ms=1000)
             else:
                 print("Wrong!")
@@ -229,7 +245,7 @@ while True:
                 target_notes = []
             else:
                 # sets next note to blue
-                setColorByIndices(strip, note_to_led_index[target_notes[len(played_notes)]], color=Color(0,0,128),wait_ms=50)
+                setColorByIndices(strip, note_to_led_index_web[target_notes[len(played_notes)]], color=Color(0,0,128),wait_ms=50)
             
         else:
             # default note playing - shows white light
