c793cea565a554c5edca322c424c02acb268558f
Enoch Huang <48170726+ehuang3190@users.noreply.github.com>
2024-03-09 15:40:35 -0800

    unprint librosa and hps

--- pyaudio_older.py
+++ pyaudio_older.py
@@ -389,8 +389,8 @@ while True:
         both_notes = list(set(both_notes))
         both_notes_string = " ".join(both_notes)
         if both_notes_string != "":
-            print("Librosa: " + l)
-            print("HPS: " + notes_hps_string)
+            #print("Librosa: " + l)
+            #print("HPS: " + notes_hps_string)
             print("Combined:" + both_notes_string)     
 
         if outputsink:
